7c5c376df9198c63f04c394ec58b208d6e0d233b
Shreepad <shreepadsoraganvi01@gamil.com>
2024-07-21 20:48:19 +0530

    test

--- 01-test.sh
+++ 01-test.sh
@@ -11,9 +11,9 @@ N="\e[0m"
 if [ $USRID -eq 0 ]
 then
     echo -e "$G you are with root user, so proceding with instation $N"
-    exit 1
 else
     echo -e "$R please shiwch to root user $N"
+    exit 1
 fi
 
 VALIDATE(){
