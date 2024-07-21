3f76a0c2cc0fb918b53e33e6c530961e6e04da48
Shreepad <shreepadsoraganvi01@gamil.com>
2024-07-21 20:31:02 +0530

    test

--- 01-test.sh
+++ 01-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USRID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -12,3 +15,22 @@ then
 else
     echo -e "$R please shiwch to root user $N"
 fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $i is already installed $N"
+        exit 1
+    else
+        echo -e "$G proceeding with the instalation of $1"
+    fi
+}
+
+for i in $@
+
+do
+    echo "Checking for the package : $i"
+    dnf list installed $i &>> LOGFILE
+    VALIDATE $? "installion of $i"
+    dnf install $i -y &>>$LOGFILE
+done
