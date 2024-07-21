2ae7d772eeb5902fc4a379aecee1706ef0a54f63
Shreepad <shreepadsoraganvi01@gamil.com>
2024-07-21 18:32:38 +0530

    test

--- 01-test.sh
+++ 01-test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 USRID=$(id -u)
-if ($USRID -eq 0)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+if ($USRID -eq 0)
 then
     echo "$G you are with root user, so proceding with instation $N"
 else
     echo "$G please shiwch to root user $N"
+if
