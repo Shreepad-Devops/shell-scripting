94409c1b7e312db97c7ddd4813654f2c6770693e
Shreepad <shreepadsoraganvi01@gamil.com>
2024-08-02 17:38:57 +0530

    opx

--- Project1/opx1_test.sh
+++ Project1/opx1_test.sh
@@ -27,14 +27,14 @@ cd /home/ec2-user/hotfix/HOTFIX/OPX/Release_$release/HF_$HF
 
 VALIDATE $? "vaidation of HF is :"
 
-for i in 'cat server_list | awk -F'=' '{print $1}''
+for i in 'cat server_list'
 do
     ssh -q ec2-user@$i "hostname";
     cd /home/ec2-user;
     mkdir test1;
 done
 
-for i in 'cat server_list | awk -F'=' '{print $1}''
+for i in 'cat server_list'
 do
     ssh -q ec2-user@$i "cd /home/ec2-user; mkdir test2;"
 done
