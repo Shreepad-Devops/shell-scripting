4a55cf334a6ff31373caafd6a3e39066dd50a15f
Shreepad <shreepadsoraganvi01@gamil.com>
2024-08-07 20:08:01 +0530

    opx

--- Project1/opx1_test.sh
+++ Project1/opx1_test.sh
@@ -39,7 +39,7 @@ touch opx"
 
 Task2=(
 	"cd /home/ec2-user/test4;
-	scp -r * ${User}@${server}:/home/ec2-user/test5/
+	scp -r * ${User}@${server}:/home/ec2-user/test5/"
 )
 for server in "${Servers[@]}"; do
 	echo "Exicuting: tasks on $server"
