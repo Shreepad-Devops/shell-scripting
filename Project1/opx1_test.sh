fd4cc8b902e299a93fbf6a6ae81b0799f1b21f50
Shreepad <shreepadsoraganvi01@gamil.com>
2024-09-04 13:22:17 +0530

    test

--- Project1/opx1_test.sh
+++ Project1/opx1_test.sh
@@ -12,6 +12,11 @@ read "HF"
 echo " please enter the release (ex: 2407)"
 read "release"
 
+Servers=("18.207.186.186"
+"44.201.234.160")
+
+User=ec2-user
+
 VALIDATE(){
 	if [ $1 -eq 0 ]
 	then
@@ -29,29 +34,24 @@ mkdir MainSystem1 MainSystem2"
 )
 
 
-Task2=("cd /home/ec2-user/test5/test3;
+Task2=("cd /home/$(User)/test5/test3;
 sh startup.sh > status.log;
 sh startuptest.sh"
 )
 
-Task3=("cd /home/ec2-user/test5/test3/MainSystem1;
+Task3=("cd /home/$(User)/test5/test3/MainSystem1;
 sh startmain1.sh > Status.txt.log;
 sh loadstatus1.sh"
 )
 
-Task4=("cd /home/ec2-user/test5/test3/MainSystem2;
+Task4=("cd /home/$(User)/test5/test3/MainSystem2;
 sh startmain2.sh > Status.txt.log;
 sh loadstatus2.sh"
 )
 
-cd /home/ec2-user/hotfix/HOTFIX/OPX/Release_$release/HF_$HF
+cd /home/$(User)/hotfix/HOTFIX/OPX/Release_$release/HF_$HF
 
 VALIDATE $? "vaidation of HF is :"
-
-Servers=("18.207.186.186"
-"44.201.234.160")
-
-User=ec2-user
 
 ##Go to the location and take the back up and create the new folder
 echo -e "$G Taking the backup and creating the new $N"
@@ -67,8 +67,8 @@ echo -e "$G copying the code OPX $N"
 for server in "${Servers[@]}"; do
 	echo "Exicuting: tasks on $server"
 	cd /home/ec2-user/hotfix/HOTFIX/OPX/Release_$release/HF_$HF
-	scp -r * ${User}@${server}:/home/ec2-user/test5/test3/MainSystem1
- 	scp -r * ${User}@${server}:/home/ec2-user/test5/test3/MainSystem2
+	scp -r * ${User}@${server}:/home/$(User)/test5/test3/MainSystem1
+ 	scp -r * ${User}@${server}:/home/$(User)/test5/test3/MainSystem2
 done
 VALIDATE $? "copy the code to OPX :"
 
